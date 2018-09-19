9449d0747c470ef7a2f4edac8554f1df029daf86
Unknown <glrd.remi@gmail.com>
2018-09-19 17:52:36 +0200

    File Controller and File databases

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,14 @@ Route::get('/test', ['uses' => "App\StaticsController@test"]);
 Route::get('/', ['uses' => "App\StaticsController@showHome"]);
 
 Route::get('/admin/login', ['uses' => "App\AdminController@showLoginAdmin"]);
-//Example de route
+
+
+//FILES
+
+Route::get('/file/{filename}', ["uses" => "App\Files\FilesController@showAFile"])->where('filename', '^[^/]+$');
+Route::post('/file/{filename}', ["uses" => "App\Files\FilesController@postFile"])->where('filename', '^[^/]+$');
+Route::get('/get/files/{token}/{password}', 'Admin\FileController@downloadFile');
+
 
 Auth::routes();
 
