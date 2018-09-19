1817e010e703bd897152299a5e7fbaf779827477
Unknown <julien.salet@devinci.fr>
2018-09-19 18:20:18 +0200

    Custom addUser

--- resources/views/app/entities/admin/addUser.blade.php
+++ resources/views/app/entities/admin/addUser.blade.php
@@ -6,20 +6,23 @@
 
 @section('content')
 
-    <form class="form-signin">
-        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-    </form>
+    <div class="container rounded bg-light row col-md-8 offset-md-2 justify-content-center text-center">
+        <form class="form-signin">
+            <h1 class="h3 mb-3 font-weight-normal text-center ">Creation d'un nouvel utilisateur</h1>
+
+            <label for="inputname" class="sr-only">Name</label>
+            <input type="name" id="inputname" class="form-control" placeholder="Nom" required autofocus>
+
+            <label for="inputfirstname" class="sr-only">firstname</label>
+            <input type="firstname" id="inputfirstname" class="form-control" placeholder="Prénom" required autofocus>
+
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required autofocus>
+
+            <button class="btn btn-secondary btn-lg" type="button">Upload</button>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Validé</button>
+        </form>
+    </div>
 @stop
 
 @section('js')
