1bb2ba7a7ae4d184bbdccf4871f6a1264c18fef8
Pierre <pierre.lebreton@hotmail.com>
2018-09-19 18:23:15 +0200

    Push pour julien

--- resources/views/app/entities/admin/login.blade.php
+++ resources/views/app/entities/admin/login.blade.php
@@ -10,11 +10,13 @@
         <div class="raw text-center">
             <form class="form-signin">
                 <h1 class="h3 mb-3 font-weight-normal">Connexion Administrateur</h1>
-                <div class="offset-md-2 col-md-8  ">
+                <div class="offset-md-2 col-md-8">
                     <label for="inputEmail" class="sr-only">Adresse email</label>
                     <input type="email" id="inputEmail" class="form-control" placeholder="Adresse email" required autofocus>
                     <label for="inputPassword" class="sr-only">Mot de passe</label>
                     <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+                    <!-- Penser à placer la route pour la récupération du mot de passe Admion ICI -->
+                    <p>Mot de passe oublié ? Cliquez <a href="PLACER UNE ROUTE ICI">ici</a></p>
                     <div class="checkbox mb-3">
                         <label>
                             <input type="checkbox" value="remember-me"> Se souvenir de moi
