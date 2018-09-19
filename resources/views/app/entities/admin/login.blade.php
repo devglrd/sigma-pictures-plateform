534e49b979c3a00dd7550270560911ec7842cc13
Pierre <pierre.lebreton@hotmail.com>
2018-09-19 18:15:04 +0200

    Admin/login template

--- resources/views/app/entities/admin/login.blade.php
+++ resources/views/app/entities/admin/login.blade.php
@@ -10,7 +10,7 @@
         <div class="raw text-center">
             <form class="form-signin">
                 <h1 class="h3 mb-3 font-weight-normal">Connexion Administrateur</h1>
-                <div class="col-md-8 ">
+                <div class="offset-md-2 col-md-8  ">
                     <label for="inputEmail" class="sr-only">Adresse email</label>
                     <input type="email" id="inputEmail" class="form-control" placeholder="Adresse email" required autofocus>
                     <label for="inputPassword" class="sr-only">Mot de passe</label>
@@ -20,7 +20,7 @@
                             <input type="checkbox" value="remember-me"> Se souvenir de moi
                         </label>
                     </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
+                <button id="btnlogin" class="btn btn-lg btn-block btn btn-outline-info" type="submit">Se connecter</button>
                 </div>
                 <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
             </form>
