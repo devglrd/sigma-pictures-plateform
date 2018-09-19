fceaeb53256b65b8cdac459167ae441ac6f2e40f
Unknown <glrd.remi@gmail.com>
2018-09-19 16:57:07 +0200

    Bootrap 4

--- resources/views/app/templates/header.blade.php
+++ resources/views/app/templates/header.blade.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://unpkg.com/notie/dist/notie.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
     @yield('css')
     @yield('top-js')
 </head>
