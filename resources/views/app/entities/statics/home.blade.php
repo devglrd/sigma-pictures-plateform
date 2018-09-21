a66fa3231c782587cbc1b73fb8507ec104dac796
Unknown <julien.salet@devinci.fr>
2018-09-21 19:00:51 +0200

    articles

--- resources/views/app/entities/statics/home.blade.php
+++ resources/views/app/entities/statics/home.blade.php
@@ -13,6 +13,72 @@
                <div class="btn">Btn</div>
                Afficher les news de la societe
             </div>
+             <div class="container">
+                 <div class="row mb-3">
+                     <div class="col rounded mr-3 bg-secondary">
+                         <div class="row">
+                            <div class="col-3 pt-3">
+                                <img src="http://www.agence-loremipsum.fr/wp-content/uploads/2017/03/logo-agence.jpg" width="100%">
+                            </div>
+                             <div class="col-9">
+                                 <div class="font-weight-bold">
+                                     Lorem Ipsum
+                                 </div>
+                                 <div>
+                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc venenatis pulvinar arcu. Proin ornare ipsum nisl, a mollis purus auctor et. Praesent eleifend molestie feugiat. Curabitur sagittis mi eu dolor egestas ornare sed eu elit.
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col rounded mr-3 bg-secondary">
+                         <div class="row">
+                             <div class="col-3 pt-3">
+                                 <img src="http://www.agence-loremipsum.fr/wp-content/uploads/2017/03/logo-agence.jpg" width="100%">
+                             </div>
+                             <div class="col-9">
+                                 <div class="font-weight-bold">
+                                     Lorem Ipsum
+                                 </div>
+                                 <div>
+                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc venenatis pulvinar arcu. Proin ornare ipsum nisl, a mollis purus auctor et. Praesent eleifend molestie feugiat. Curabitur sagittis mi eu dolor egestas ornare sed eu elit.
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="row mb-3">
+                     <div class="col rounded mr-3 bg-secondary">
+                         <div class="row">
+                             <div class="col-3 pt-3">
+                                 <img src="http://www.agence-loremipsum.fr/wp-content/uploads/2017/03/logo-agence.jpg" width="100%">
+                             </div>
+                             <div class="col-9">
+                                 <div class="font-weight-bold">
+                                     Lorem Ipsum
+                                 </div>
+                                 <div>
+                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc venenatis pulvinar arcu. Proin ornare ipsum nisl, a mollis purus auctor et. Praesent eleifend molestie feugiat. Curabitur sagittis mi eu dolor egestas ornare sed eu elit.
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col rounded mr-3 bg-secondary">
+                         <div class="row">
+                             <div class="col-3 pt-3">
+                                 <img src="http://www.agence-loremipsum.fr/wp-content/uploads/2017/03/logo-agence.jpg" width="100%">
+                             </div>
+                             <div class="col-9">
+                                 <div class="font-weight-bold">
+                                     Lorem Ipsum
+                                 </div>
+                                 <div>
+                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc venenatis pulvinar arcu. Proin ornare ipsum nisl, a mollis purus auctor et. Praesent eleifend molestie feugiat. Curabitur sagittis mi eu dolor egestas ornare sed eu elit.
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
          </div>
       </div>
    </section>
